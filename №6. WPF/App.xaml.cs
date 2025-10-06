cc8b33aa2e4eeb1b8aafd59b4d760aee917a58bc
Максим Шмелев <smelevmaksim667@gmail.com>
2025-10-06 15:15:20 +0500

    Загрузочка проекта

---
+++ №6. WPF/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace _6._WPF
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
